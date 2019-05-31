ee0105aa37e9b0178ef98ebd2f066e04e8b9b8b6
Tihomir Iliev <Tihomir.Iliev@ipsos.com>
2019-05-31 13:39:37 +0300

    adding the projects page

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: "Projects page",
+        welcome: "Welcome to our projects page",
+        content: "List of the projects"
+    })
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: "Unable to handle request"
