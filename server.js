2fef314179ddf2741358a5e53a963147b0b1349a
Tihomir Iliev <Tihomir.Iliev@ipsos.com>
2019-05-31 09:40:59 +0300

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     var now = new Date().toString();
@@ -20,9 +19,11 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
